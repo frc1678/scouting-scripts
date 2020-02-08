c643f998049f53fdf47fc7c01f227b4d9697300a
Carl Csaposs <32380359+ccsaposs@users.noreply.github.com>
2020-02-08 10:20:02 -0800

    Create ubuntu_install.sh

---
+++ ubuntu_install.sh
@@ -0,0 +1,6 @@
+sudo apt-get remove -y ubuntu-web-launchers rhythmbox* deja-dup gnome-mines gnome-mahjongg gnome-sudoku shotwell* thunderbird*
+sudo apt-get autoremove -y
+sudo apt-get install -y git
+sudo snap install --classic android-studio 
+sudo snap install --classic slack 
+sudo snap install --classic pycharm-community
