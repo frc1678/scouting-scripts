3b4dc10ba6916caa3bd527701d57fb91268c3c94
Carl Csaposs <32380359+ccsaposs@users.noreply.github.com>
2020-02-08 19:13:16 -0800

    Update and rename ubuntu_install.sh to server_laptop_install.sh

--- server_laptop_install.sh
+++ server_laptop_install.sh
@@ -1,6 +1,11 @@
+# Designed for use on Ubuntu 18.04 LTS
+# Remove unused programs
 sudo apt-get remove -y ubuntu-web-launchers rhythmbox* deja-dup gnome-mines gnome-mahjongg gnome-sudoku shotwell* thunderbird*
 sudo apt-get autoremove -y
-sudo apt-get install -y git
+# Install needed programs
+sudo apt-get install -y git mongodb android-tools-adb python3-venv
 sudo snap install --classic android-studio 
 sudo snap install --classic slack 
 sudo snap install --classic pycharm-community
+# Save git password
+git config credential.helper store
